86eda9914c3b897763caa9e022712778c057b030
A20160100 <A20160100@juan23.edu.pe>
2025-09-30 11:08:54 -0500

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,20 @@
+for(i=1; i<=5; i++){
+    document.writeln("Mono<br>");
+}
+document.writeln("<br>")
+for(i=1; i<=10; i++){
+    document.writeln("i"+",")
+}
+document.writeln("<br>")
+for(i=1; i<=8; i++){
+    document.writeln(i*3+"-")
+}
+document.writeln("<br>")
+for(i=1; i=7; i++){
+    document.writeln(i*7)
+}
+document.writeln("<br>")
+for(i=1; i<=10; i++){
+    document.writeln(i,"x", 6, "=", i*6)
+}
+document.writeln("<br>")
